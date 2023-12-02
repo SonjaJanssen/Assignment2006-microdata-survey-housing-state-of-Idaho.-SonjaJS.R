d3d4b45e5493ad7c99aadc40dc3061054cb8b879
Sonja Janssen-Sahebzad <123073089+SonjaJanssen@users.noreply.github.com>
2023-12-02 16:29:13 +0100

    Update 2006 microdata survey  housing state of Idaho. SonjaJS.R

--- 2006 microdata survey  housing state of Idaho. SonjaJS.R
+++ 2006 microdata survey  housing state of Idaho. SonjaJS.R
@@ -1,7 +1,7 @@
 # Data Cleaning Assignment
 # 2006 Microdata Survey on Housing in Idaho
 # Date: March 22, 2023
-# Code by: Sonja Janssen-Sahebzad
+# Code by: SonjaJS
 
 # ----------------------------------------------------------------------------------------------------- #
 
@@ -23,9 +23,15 @@ housing_data <- fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss
 high_value_properties <- housing_data[VAL == 24, .N]
 # Answer: 53
 
+# Explanation: This code uses the data.table package to efficiently read and manipulate the housing data.
+# The question involves selecting rows where the property value is greater than $1,000,000 and counting them.
+
 # ----------------------------------------------------------------------------------------------------- #
 # Question 2: Consider the variable FES in the code book. Which "tidy data" principle does this variable violate?
 # Answer: Tidy data has one variable per column.
+
+# Explanation: Tidy data principles state that each variable should have its own column.
+# If FES violates this, it means it might contain information that could be better organized.
 
 # ----------------------------------------------------------------------------------------------------- #
 # Question 3: Download the Excel spreadsheet on Natural Gas Acquisition Program:
@@ -37,6 +43,9 @@ download.file(file_url, destfile = paste0(getwd(), '/getdata%2Fdata%2FDATA.gov_N
 ngap_data <- fread(file = "getdata%2Fdata%2FDATA.gov_NGAP.xlsx", sheetIndex = 1, rowIndex = 18:23, colIndex = 7:15)
 sum_zip_ext <- sum(ngap_data$Zip * ngap_data$Ext, na.rm = TRUE)
 # Answer: 36,534,720
+
+# Explanation: This code downloads an Excel file, reads a specific range of rows and columns, 
+# and then calculates the sum of Zip * Ext, presumably for the Natural Gas Acquisition Program.
 
 # ----------------------------------------------------------------------------------------------------- #
 # Question 4: Read the XML data on Baltimore restaurants:
@@ -53,6 +62,9 @@ restaurants_21231 <- data.table::data.table(zipcode = zipcodes)
 num_restaurants_21231 <- restaurants_21231[zipcode == "21231", .N]
 # Answer: 127
 
+# Explanation: This code reads XML data on Baltimore restaurants and extracts information about the zipcodes.
+# The result is a count of restaurants with the specified zipcode.
+
 # ----------------------------------------------------------------------------------------------------- #
 # Question 5: Download the 2006 microdata survey about housing in Idaho:
 # Data: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
@@ -65,8 +77,12 @@ DT <- fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
 # Answer: Use system.time() to measure the time taken for the calculation.
 calculation_time <- system.time(DT[, mean(pwgtp15), by = SEX])
 
+# Explanation: This code downloads another dataset, loads it into R, 
+# and then calculates the average value of the variable pwgtp15, broken down by sex.
+# system.time() is used to measure the time taken for this operation.
+
 # ----------------------------------------------------------------------------------------------------- #
 
-
+# Explanation: The code is marked as 100% by peer review, indicating successful completion.
 
 
