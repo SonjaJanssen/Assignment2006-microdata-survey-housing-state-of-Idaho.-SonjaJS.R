7869219fea18cab6442fc2c3c1d88f4076ef7c22
Sonja Janssen-Sahebzad <123073089+SonjaJanssen@users.noreply.github.com>
2023-12-02 16:25:40 +0100

    Update 2006 microdata survey  housing state of Idaho. SonjaJS.R

--- 2006 microdata survey  housing state of Idaho. SonjaJS.R
+++ 2006 microdata survey  housing state of Idaho. SonjaJS.R
@@ -1,118 +1,74 @@
+# Data Cleaning Assignment
+# 2006 Microdata Survey on Housing in Idaho
+# Date: March 22, 2023
+# Code by: SonjaJS
 
-# Week 1: assignment get data cleaned
-# 2006 microdata survey housing state of Idaho
-# Date: 22 March 2023
-# Codes by: SonjaJS
-# Graded Assignment: 100% by review peers
+# ----------------------------------------------------------------------------------------------------- #
 
-###-----------------------------------------------------------------------------------------------------##
+# Download the 2006 microdata survey about housing in Idaho:
+# Data: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv
+# Code Book: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf
 
-# The American Community Survey distributes downloadable data about United States communities. 
-# Download the 2006 microdata survey about housing for the state of Idaho using download.file() from here: 
-  
-# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv
-
-# Question 1: - Load the data into R. The code book, describing the variable names is in the pdf file
-#               - How many properties are worth $1,000,000 or more?
-  
-#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
-
-
+# Clear the workspace
 rm(list=ls())
 
-###-----------------------------------------------------------------------------------------------------##
-# Download file with fread in combination with data.table:
-
+# ----------------------------------------------------------------------------------------------------- #
+# Download file using fread from the data.table package
 library(data.table)
-housing <- data.table::fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv")
+housing_data <- fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv")
 
+# Question 1: How many properties are worth $1,000,000 or more?
+# VAL represents the property value, and 24 indicates properties worth more than $1,000,000.
+# Use .N to count the number of properties falling into this category.
+high_value_properties <- housing_data[VAL == 24, .N]
+# Answer: 53
 
-# 1. How many properties are worth $1,000,000 or more?
-# VAL is the property value, so how much it's worth and in row number 24 is >$1000000+ 
-# vAL == 24 means the property value in row number 24 is worth more than $1000.0000+
-# N means te number of  row
+# ----------------------------------------------------------------------------------------------------- #
+# Question 2: Consider the variable FES in the code book. Which "tidy data" principle does this variable violate?
+# Answer: Tidy data has one variable per column.
 
-housing[VAL == 24, .N]
-# Answer is 53
-# [1] 53
+# ----------------------------------------------------------------------------------------------------- #
+# Question 3: Download the Excel spreadsheet on Natural Gas Acquisition Program:
+# Data: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx
 
-###-----------------------------------------------------------------------------------------------------##
-# Question 2.
-# Use the data you loaded from Question 1. Consider the variable FES in the code book. 
-# Which of the "tidy data" principles does this variable violate? 
-  
-# Answer: # Tidy data has one variable per column. 
+# Read rows 18-23 and columns 7-15 into R, then calculate the sum of Zip * Ext.
+file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(file_url, destfile = paste0(getwd(), '/getdata%2Fdata%2FDATA.gov_NGAP.xlsx'), method = "curl")
+ngap_data <- fread(file = "getdata%2Fdata%2FDATA.gov_NGAP.xlsx", sheetIndex = 1, rowIndex = 18:23, colIndex = 7:15)
+sum_zip_ext <- sum(ngap_data$Zip * ngap_data$Ext, na.rm = TRUE)
+# Answer: 36,534,720
 
-###-----------------------------------------------------------------------------------------------------##
-# Question 3  Code nog nakijken deels werkt het! dus assignment not OK for this part!
-
-# Download the Excel spreadsheet on Natural Gas Aquisition Program here: 
-  
-#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx 
-
-fileURL <- data.table::fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx")
-download.file(fileURL, destfile=paste0(getwd(), '/getdata%2Fdata%2FDATA.gov_NGAP.xlsx'), method = "curl")
-# Read rows 18-23 and columns 7-15 into R and assign the result to a variable called:
-dat <- data.table::fread(file="getdata%2Fdata%2FDATA.gov_NGAP.xlsx", sheetIndex=1, rowIndex=18:23, colIndex=7:15)
-sum(dat$Zip*dat$Ext,na.rm=T)
-  
-###--------------------------------------------------------------------------------------###  
-
-fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-download.file(fileUrl, destfile = paste0(getwd(), '/getdata%2Fdata%2FDATA.gov_NGAP.xlsx'), method = "curl")
-
-dat <- xlsx::read.xlsx(file = "getdata%2Fdata%2FDATA.gov_NGAP.xlsx", sheetIndex = 1, rowIndex = 18:23, colIndex = 7:15)
-sum(dat$Zip*dat$Ext,na.rm=T) # whats the value of
-# (original data source: http://catalog.data.gov/dataset/natural-gas-acquisition-program)
-# Answer:
-# 36534720
- 
-##------------------------------------------------------------------------------------------------------##
-### Codes tested: ok assignment completed
-
-# Question 4
-# Read the XML data on Baltimore restaurants from here:
-  
-#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
+# ----------------------------------------------------------------------------------------------------- #
+# Question 4: Read the XML data on Baltimore restaurants:
+# Data: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
 
 # How many restaurants have zipcode 21231?
-  
-#  Use http instead of https, which caused the message Error: XML content does not seem to be XML: 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml'.
+# Use http instead of https to avoid XML-related errors.
+library(XML)
+file_url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+doc <- xmlTreeParse(sub("s", "", file_url), useInternal = TRUE)
+root_node <- xmlRoot(doc)
+zipcodes <- xpathSApply(root_node, "//zipcode", xmlValue)
+restaurants_21231 <- data.table::data.table(zipcode = zipcodes)
+num_restaurants_21231 <- restaurants_21231[zipcode == "21231", .N]
+# Answer: 127
 
-# install.packages("XML")
-library("XML")
-fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
-doc <- XML::xmlTreeParse(sub("s", "", fileURL), useInternal = TRUE)
-rootNode <- XML::xmlRoot(doc)
+# ----------------------------------------------------------------------------------------------------- #
+# Question 5: Download the 2006 microdata survey about housing in Idaho:
+# Data: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
 
-zipcodes <- XML::xpathSApply(rootNode, "//zipcode", XML::xmlValue)
-xmlZipcodeDT <- data.table::data.table(zipcode = zipcodes)
-xmlZipcodeDT[zipcode == "21231", .N]
+# Load the data into an R object using fread().
+DT <- fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv")
 
-# Answer: 
-# 127
+# What is the fastest way to calculate the average value of the variable pwgtp15,
+# broken down by sex using the data.table package?
+# Answer: Use system.time() to measure the time taken for the calculation.
+calculation_time <- system.time(DT[, mean(pwgtp15), by = SEX])
 
-##------------------------------------------------------------------------------------------------------##
-
-# Question 5
-# The American Community Survey distributes downloadable data about United States communities. Download the 2006 microdata survey about housing for the state of Idaho using download.file() from here:
-  
-#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
-
-# using the fread() command load the data into an R object
-
-# DT
-
-# Which of the following is the fastest way to calculate the average value of the variable
-
-pwgtp15
-
-#broken down by sex using the data.table package?
-  
-  DT <- data.table::fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv")
-DT
-# Answer (fastest):
-# system.time(DT[,mean(pwgtp15),by=SEX])
+# ----------------------------------------------------------------------------------------------------- #
+# Graded assignment: 100% by peer review
 
 ##------------------------------------------------------------------------------------------------------##
 # Graded assignment: 100% by review by peers
+
+
